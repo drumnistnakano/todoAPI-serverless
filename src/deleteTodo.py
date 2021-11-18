5e68e38ee79d8e179dd53efdd074d4c386f2fdf6
drumnistnakano <syxqh349ysykn@gmail.com>
2021-11-19 07:58:07 +0900

    Add testcode of deleteTodo function

--- src/deleteTodo.py
+++ src/deleteTodo.py
@@ -1,13 +1,13 @@
 import json
-import random
 import boto3
-import string
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('TodoTable')
 
 def delete_todo(event, context):
-    id = event["pathParameters"]["id"]
-    table.delete_item(Key={"id": id})
-    response = {"statusCode": 200, "body": json.dumps({"id": id})}
+    user_id = event["pathParameters"]["userId"]
+    body = json.loads(event.get("body"))
+    todo_id = body.get("todoId")
+    table.delete_item(Key={"userId": user_id, "todoId": todo_id})
+    response = {"statusCode": 200, "body": json.dumps({"userId": user_id, "todoId": todo_id})}
     return response
---
+++ tests/test_deleteTodo.py
@@ -0,0 +1,68 @@
+import boto3
+import pytest
+import os
+import yaml
+import json
+from moto import mock_dynamodb2
+
+@pytest.mark.parametrize(
+    "event, expected",
+    [
+        (
+            {
+                "pathParameters": {
+                    "userId": "0001",
+                },
+                "body": json.dumps(
+                    {
+                        "todoId": "0001",
+                    }
+                )
+            },
+            {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "userId": "0001",
+                        "todoId": "0001"
+                    }
+                )
+            }
+        )
+    ]
+)
+
+def test_update_todo(table, event, expected):
+    from src.deleteTodo import delete_todo
+    assert expected == delete_todo(event, context=None)
+
+@pytest.fixture(autouse=True)
+def set_envs(monkeypatch):
+    with open('tests/test_data.yml', 'r', encoding='utf-8') as fp:
+        envs = yaml.safe_load(fp)['environment']
+ 
+    for k, v in envs.items():
+        monkeypatch.setenv(k, str(v))
+ 
+@pytest.fixture()
+def table():
+    with mock_dynamodb2():
+        # テスト用テーブル定義の読み込み
+        with open('tests/test_data.yml', 'r', encoding='utf-8') as fp:
+            test_data = yaml.safe_load(fp)['DynamoDB']
+ 
+        table_config = test_data['Table']
+        dynamodb = boto3.resource('dynamodb')
+        # テスト用テーブルの作成
+        dynamodb.create_table(
+            TableName=os.environ['TableName'],
+            AttributeDefinitions=table_config['AttributeDefinitions'],
+            KeySchema=table_config['KeySchema']
+        )
+        # テスト用データの格納
+        table = dynamodb.Table(os.environ['TableName'])
+        with table.batch_writer() as batch:
+            for item in test_data['Items']:
+                batch.put_item(Item=item)
+ 
+        yield table
