bcec57117ab0931affe5111c0b483166e087b07a
drumnistnakano <syxqh349ysykn@gmail.com>
2021-11-28 21:14:09 +0900

    Add exception handling for each lambda

--- src/createTodo.py
+++ src/createTodo.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+import botocore
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
@@ -14,6 +15,12 @@ def create_todo(event, context):
 
     item = {"userid": user_id, "todoid": todo_id, "title": title, "content": content}
     
-    table.put_item(Item=item)
-
+    try:
+        table.put_item(Item=item)
+    except botocore.exceptions.ClientError as e:
+        print(e)
+        raise e
+    except botocore.exceptions.ParamValidationError as e:
+        raise ValueError('The parameters you provided are incorrect: {}'.format(e))
+    
     return {"statusCode": 200, "body": json.dumps(item), 'headers': {"Access-Control-Allow-Origin": "*"}}
--- src/getTodo.py
+++ src/getTodo.py
@@ -2,6 +2,7 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Key
 import os
+import botocore
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
@@ -9,11 +10,20 @@ table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 def get_todo(event, context):
     user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
 
-    if event["pathParameters"] is None:
-        allusers_todo = table.query(KeyConditionExpression=Key("userid").eq(user_id))
-        item = allusers_todo.get("Items")
-    else:
-        todo = table.get_item(Key={"userid": user_id, "todoid": event["pathParameters"]["todoid"]})
-        item = todo.get("Item")
+    try:
+        if event["pathParameters"] is None:
+            # クエリ文字列がない場合、ユーザの全てのTodoを取得
+            result = table.query(KeyConditionExpression=Key("userid").eq(user_id))
+            item = result.get('Items')
+        else:
+            # クエリ文字列がある場合、ユーザの単一のTodoを取得
+            result = table.get_item(Key={"userid": user_id, "todoid": event["pathParameters"]["todoid"]})
+            item = result.get('Item')
+    except botocore.exceptions.ClientError as e:
+        print(e)
+        raise e
+    except botocore.exceptions.ParamValidationError as e:
+        raise ValueError('The parameters you provided are incorrect: {}'.format(e))
 
+    print(item)
     return {"statusCode": 200, "body": json.dumps(item),'headers': {"Access-Control-Allow-Origin": "*"}}
